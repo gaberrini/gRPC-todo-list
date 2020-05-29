47f62e9b39ffde8cf2d28c6ef07e63b44b8fe229
Gaston Lucero <gaberrini@hotmail.com>
2020-05-28 21:02:20 -0300

    raise exception on error

--- src/proto_client/todolists_client.py
+++ src/proto_client/todolists_client.py
@@ -35,6 +35,7 @@ def create_list(name: str, channel: Channel) -> Optional[todolists_pb2.CreateLis
             print(e.args[0].details)
         else:
             print(e.args[0].details)
+        raise e
 
 
 if __name__ == '__main__':
